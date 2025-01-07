f60f75ef37daa70fa6cef6d4b7cf0d0ab9170ae4
AssumartLabDynamics <assumartlabdynamics@gmail.com>
2025-01-07 13:27:12 +0000

    Create firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCsmgZiBLi1WMFfWEm3NbXSgPHdnchMErI",
+  authDomain: "chat-app-b8386.firebaseapp.com",
+  projectId: "chat-app-b8386",
+  storageBucket: "chat-app-b8386.firebasestorage.app",
+  messagingSenderId: "918056477438",
+  appId: "1:918056477438:web:2ac7beaa4b8509685cbbaf",
+  measurementId: "G-GNFM79N8Q5"
+};
+
+export default firebaseConfig;
