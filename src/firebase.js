20c0b001e0176990f19350a763c2985b680c9ff3
AssumartLabDynamics <assumartlabdynamics@gmail.com>
2025-01-07 13:32:20 +0000

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,10 @@
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCsmgZiBLi1WMFfWEm3NbXSgPHdnchMErI",
-  authDomain: "chat-app-b8386.firebaseapp.com",
-  projectId: "chat-app-b8386",
-  storageBucket: "chat-app-b8386.firebasestorage.app",
-  messagingSenderId: "918056477438",
-  appId: "1:918056477438:web:2ac7beaa4b8509685cbbaf",
-  measurementId: "G-GNFM79N8Q5"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 export default firebaseConfig;
